af7f7eb0b4757ea9d7d051ef2ca50998b5a06fa3
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2020-01-03 19:22:50 +0000

    Installed Websockets

--- game/game.js
+++
@@ -1,8 +0,0 @@
-var gameModule = angular.module("game", ["ngRoute"]);
-
-gameModule.component("game", { 
-	templateUrl: "game/game.template.html", 
-	controller: function gameController($http) {
-		var self = this; var reqUrl = "http://localhost:9000/game"; $http.get(reqUrl).then(function(res){})
-	}
-});
