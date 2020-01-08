f8c99adc7b605b7c74694d0a1a49110ac8274089
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2020-01-08 17:24:43 +0000

    Updated test-rps.js
    Added a test suite which tests the main game logic to make sure it functions correctly

--- test/test-rps.js
+++ test/test-rps.js
@@ -1,8 +1,46 @@
-var rpsServer = require("../server");
-var assert = require("chai").assert;
+var chai = require("chai");
+var rpsGame = require("../game-engine");
+var turns = rpsGame.turns;
 
-suite("", function() {
-  test("", function() {
-
-  });
+suite("Test the game logic", function() {
+	test("Calculate the turn", function() {
+		chai.assert.equal(calculateTurn('rock'), 0, "Turn is invalid");
+	});	
+	test("Test the distance calculation", function() {
+		var test1 = calculateTurn('rock'); // 0
+		var test2 = calculateTurn('paper');// 2
+		chai.assert.equal((test1 - test2 + 3) % 3, 1, "Calculation error");
+	});
+	test("Test if it's a draw", function() {
+		var testCalc = (2 - 2 + 3) % 3; // 0
+		chai.assert.isTrue(testDraw(testCalc), true, "This is not a draw")
+	});	
 });
+
+function calculateTurn(turn)
+    {
+      switch (turn)
+      {
+        case 'rock':
+          return 0;
+        case 'scissors':
+          return 1;
+        case 'paper':
+          return 2;
+        default:
+          throw new Error("Couldn't decode turn!" + turn);
+      }
+
+    }
+	
+function testDraw(dist)
+	{
+		switch(dist)
+		{
+			case 0:
+				return true;
+			default:
+				return false;
+		}
+	}
+
